a2d2d03cd6f92c782e75eb88501011e922d33926
Rocco <rocco.bugs@yahoo.com>
2022-12-22 09:50:25 +0100

    Update default.py

--- default.py
+++ default.py
@@ -17,31 +17,31 @@ urlrthk4 = "https://rthk.hk/live4.m3u"
 urlrthk5 = "https://rthk.hk/live5.m3u"
 urlpth = "https://rthk.hk/livepth.m3u"
 #
-item21 = xbmcgui.ListItem('881 WIP', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/881.png')
+item21 = xbmcgui.ListItem('881 WIP', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/881.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), url881, item21, isFolder=0)
 
-item22 = xbmcgui.ListItem('903 WIP', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/903.png')
+item22 = xbmcgui.ListItem('903 WIP', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/903.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), url903, item22, isFolder=0)
 
-item23 = xbmcgui.ListItem('864 WIP', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/864.png')
+item23 = xbmcgui.ListItem('864 WIP', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/864.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), url864, item23, isFolder=0)     
 
-item24 = xbmcgui.ListItem('RTHK 1, iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/radio1.png')
+item24 = xbmcgui.ListItem('RTHK 1, iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/radio1.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlrthk1, item24, isFolder=0)     
 
-item25 = xbmcgui.ListItem('RTHK 2', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/radio2.png')
+item25 = xbmcgui.ListItem('RTHK 2', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/radio2.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlrthk2, item25, isFolder=0)   
 
-item26 = xbmcgui.ListItem('RTHK 3', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/radio3.png')
+item26 = xbmcgui.ListItem('RTHK 3', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/radio3.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlrthk3, item26, isFolder=0)   
 
-item27 = xbmcgui.ListItem('RTHK 4', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/radio4.png')
+item27 = xbmcgui.ListItem('RTHK 4', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/radio4.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlrthk4, item27, isFolder=0)   
 
-item28 = xbmcgui.ListItem('RTHK 5', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/radio5.png')
+item28 = xbmcgui.ListItem('RTHK 5', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/radio5.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlrthk5, item28, isFolder=0)   
 
-item29 = xbmcgui.ListItem('PTH', iconImage='special://home/addons/plugin.audio.hongkongradio/resources/media/pth.png')
+item29 = xbmcgui.ListItem('PTH', iconImage="special://home/addons/plugin.audio.hongkongradio/resources/media/pth.png")
 xbmcplugin.addDirectoryItem(int(sys.argv[1]), urlpth, item29, isFolder=0)   
 
 xbmcplugin.endOfDirectory(pluginhandle)
